cf586247f14e2e280fb191fef8c3f9368de9da5a
1TheCrazy <3thecrazy@gmail.com>
2026-07-18 12:48:33 +0200

    add wireguard detection

--- crates/core/src/wireguard/mod.rs
+++ crates/core/src/wireguard/mod.rs
@@ -1,2 +1,3 @@
 pub mod node;
 pub mod common;
+pub mod install;
--- crates/node/src/main.rs
+++ crates/node/src/main.rs
@@ -2,9 +2,16 @@ mod http;
 mod util;
 
 use crate::http::{http_server::HttpServer, state::SharedServer};
+use tunnel_core::wireguard::install;
 
 #[tokio::main]
 async fn main() {
+    let wireguard_installed = install::is_wireguard_available();
+
+    if !wireguard_installed {
+        panic!("Wireguard not installed.\nInstalling wireguard through Tunnel is currently not supported.")
+    }
+
     let http_server = SharedServer::from_config();
     
     // Start server
